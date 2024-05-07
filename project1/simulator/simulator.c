ed599536ac7a2c77b8d2ac49b4ba73e597f01509
jin <eve7777@hanyang.ac.kr>
2024-05-07 16:54:51 +0900

    simulator.c run

--- project1/simulator/simulator.c
+++ project1/simulator/simulator.c
@@ -6,7 +6,8 @@
 
 #define NUMMEMORY 65536 /* maximum number of words in memory */
 #define NUMREGS 8 /* number of machine registers */
-#define MAXLINELENGTH 1000 
+#define MAXLINELENGTH 1000
+
 typedef struct stateStruct {
     int pc;
     int mem[NUMMEMORY];
@@ -16,6 +17,10 @@ typedef struct stateStruct {
 
 void printState(stateType *);
 int convertNum(int num);
+
+void parse(stateType *, int *, int *, int *, int *);
+void testReg(int regNum);
+void testOffset(int offset);
 
 int main(int argc, char *argv[])
 {
@@ -46,8 +51,105 @@ int main(int argc, char *argv[])
         printf("memory[%d]=%d\n", state.numMemory, state.mem[state.numMemory]);
     }
 
-		/* TODO: */
-    return(0);
+    /* TODO */
+	// register initialize to 0
+    for (int x=0; x<NUMREGS; x++){
+        state.reg[x] =0;
+    }
+
+    int halted=0;
+    int instruction_cnt=0;
+    while(1){
+        printState(&state);
+        if (halted==1) break;
+        int opcode, arg0, arg1, arg2;
+        parse(&state, &opcode, &arg0, &arg1, &arg2);
+        
+        state.pc++;
+        /* opcode : add(0), nor(1), lw(2), sw(3), beq(4), jalr(5), halt(6), noop(7) */
+        /* R-type */
+        if (opcode==0 || opcode==1){
+            testReg(arg0);
+            testReg(arg1);
+            testReg(arg2);
+            // add
+            if (opcode==0){
+                state.reg[arg2] = state.reg[arg0] + state.reg[arg1];
+            }
+            // nor : or 연산에 not
+            else if (opcode==1){
+                state.reg[arg2] = ~(state.reg[arg0] || state.reg[arg1]);
+            }
+            instruction_cnt++;
+        }
+        /* I-type */
+        else if (opcode==2 || opcode==3 || opcode==4){
+            int offset = convertNum(arg2);
+            testReg(arg0);
+            testReg(arg1);
+            testOffset(offset);
+            /* lw
+                lw regA(arg0) regB(arg1) offset(arg2)
+                Load regB from memory(offset+A).
+                Memory address is formed by adding offsetField with the contents of regA.
+            */
+            if (opcode==2){
+                state.reg[arg1] = state.mem[state.reg[arg0] + offset];
+            }
+            /* sw
+                sw regA(arg0) regB(arg1) offset(arg2)
+                Store regB into memory(offset+A).
+                Memory address is formed by adding offsetField with the contents of regA.
+            */
+            else if (opcode==3){
+                state.mem[state.reg[arg0]+ offset] = state.reg[arg1];
+            }
+            /* beq
+                beq regA regB offset
+                if the contents of regA and regB are the same,
+                then branch to the address PC+1+offsetField,
+                where PC is the address of this beq instruction.
+            */
+            else if (opcode==4){
+                if (state.reg[arg0]==state.reg[arg1]){
+                    state.pc += offset;
+                }
+            }
+            instruction_cnt++;
+        }
+        /* J-type */
+        // First, store PC+1 into regB, where PC is the address of this "jalr" instruction.
+        // Then branch to the address contained in regA.
+        // Note that this implies if regA and regB refer to the same register, the net effect will be jumping to Pc+1.
+        else if (opcode==5){
+            testReg(arg0);
+            testReg(arg1);
+            state.reg[arg1] = state.pc;
+            state.pc = state.reg[arg0];
+            instruction_cnt++;
+        }
+        /* O-type */
+        else if (opcode==6 || opcode==7){
+            if (opcode==6){ // halt
+                halted=1;
+                instruction_cnt++;
+                break;
+            }
+            else if (opcode==7){ // noop
+                instruction_cnt++;
+            }
+        }
+        else{
+            printf("error: Unrecognized opcodes\n");
+            exit(1);
+        }
+    }
+    printf("machine halted\n");
+    printf("total of %d instructions executed\n", instruction_cnt);
+    printf("final state of machine:\n");
+    printState(&state);
+    fclose(filePtr);
+    exit(0);
 }
 
 void printState(stateType *statePtr)
@@ -73,4 +175,33 @@ int convertNum(int num)
 		num -= (1 << 16);
 	}
 	return (num);
+}
+
+//* new!
+void parse(stateType *statePtr, int *opcode, int *arg0, int *arg1, int *arg2){
+    int currentMemory = statePtr->mem[statePtr->pc];
+    /*
+        opcode : bit 24-22
+        fld0 : bit 21-19
+        fld1 : bit 18-16
+        fld2 : bit 15-0
+    */
+    *opcode = (currentMemory >> 22) & 0b111;
+    *arg0 = (currentMemory >> 19) & 0b111;
+    *arg1 = (currentMemory >> 16) & 0b111;
+    *arg2 = currentMemory & 0xffff;
+}
+
+void testReg(int regNum){
+	if (regNum < 0 || regNum > 7){
+		printf("error: Register outside the range [0,7], input regNum is %d\n", regNum);
+		exit(1);
+	}
+}
+
+void testOffset(int offset){
+    if (offset > 32767 || offset < -32768){
+        printf("error: offsetFields that don't fit in 16 bits\n");
+        exit(1);
+    }
 }
